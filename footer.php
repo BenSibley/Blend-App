661e765a0f4ff3e67100d0070364c46d79aef5f2
BenSibley <ben@competethemes.com>
2015-03-05 14:12:13 -0500

    updated footer and added styles for About page

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 
 <footer class="site-footer" role="contentinfo">
     <div class="contact-us">
-        <p>Have any questions? We'd love to chat <script type='text/javascript'>var a = new Array('m','team@backli','nksentry.co');document.write("<a href='mailto:"+a[1]+a[2]+a[0]+"'>"+a[1]+a[2]+a[0]+"</a>");</script></p>
+        <p>Copyright © <?php echo date('Y'); ?> <a href="http://blendapp.io">Blendapp.io</a>. All Rights Reserved</p>
     </div>
 </footer>
 
